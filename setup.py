6ca0c70ef513eb12acd21a1a9ffa3232256c4fa7
Michael Clerx <michael.clerx@nottingham.ac.uk>
2023-06-14 15:01:07 +0100

    And another 0.0.2

--- setup.py
+++ setup.py
@@ -2,10 +2,22 @@
 
 from setuptools import find_packages, setup, Extension
 
+
+# Load text for description and license
+with open('README.md') as f:
+    readme = f.read()
+
+
 setup(
     name='myokit_beta',
 
+    # Version
     version='0.0.2',
+
+    # Description
+    description='Trying out distribution of wheels etc and llvm',
+    long_description=readme,
+    long_description_content_type='text/markdown',
 
     # Packages to include
     packages=find_packages(include=('myokit_beta', 'myokit_beta.*')),
