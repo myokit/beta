2ba49d5fda1d7956ed812e85baf89e9dbdd38225
Michael Clerx <michael.clerx@nottingham.ac.uk>
2023-06-14 14:34:42 +0100

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup, Extension
 setup(
     name='myokit_beta',
 
-    version='0.0.1',
+    version='0.0.2',
 
     # Packages to include
     packages=find_packages(include=('myokit_beta', 'myokit_beta.*')),
