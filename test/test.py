3fe140db02c0caf7e6b567529d741639c6dd7589
Michael Clerx <michael.clerx@nottingham.ac.uk>
2023-06-15 12:45:29 +0100

    mac

--- test/test.py
+++ test/test.py
@@ -3,7 +3,11 @@ print('GOING TO IMPORT')
 
 import myokit_beta
 
-#print(myokit_beta.hi())
+print('GOING TO SAY HI')
+
+print(myokit_beta.hi())
+
+print('SAID HI')
 #print(myokit_beta.sum())
 
 #assert myokit_beta.sum() == 12.345
